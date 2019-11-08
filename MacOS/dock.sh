5474c0242cf4377bd76c8049caa60ff6ba86e44e
Aryan Jabbari <aryan@me0cbbcb521a4.localdomain>
2019-11-08 17:34:22 -0500

    Update work dotfiles

--- MacOS/dock.sh
+++ MacOS/dock.sh
@@ -1,14 +1,14 @@
 dockutil --remove all
 
-dockutil --add 	/Applications/Spark.app/
-dockutil --add  /Applications/Brave%20Browser.app/
-dockutil --add  /Applications/App%20Store.app/
+dockutil --add 	/Applications/Mail.app/
+dockutil --add  "/Applications/Brave Browser.app/"
+dockutil --add  "/Applications/App Store.app/"
 dockutil --add 	/Applications/Bear.app/
 dockutil --add 	/Applications/Calendar.app/
-dockutil --add  /Applications/System%20Preferences.app/
+dockutil --add  "/Applications/System Preferences.app/"
 dockutil --add 	/Applications/Utilities/Terminal.app/
 dockutil --add 	/Applications/Slack.app/
 dockutil --add 	/Applications/LastPass.app/
 dockutil --add 	/Applications/Spotify.app/
-dockutil --add  /Applications/Visual%20Studio%20Code.app/
+dockutil --add  "/Applications/Visual Studio Code.app/"
 dockutil --add 	/Applications/Insomnia.app/

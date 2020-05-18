5d80eb67ce0065ac0003621ad0411bc0c01ec2cf
Aryan Jabbari <AryanJabbari@gmail.com>
2020-05-18 10:35:19 -0400

    Update dock.sh

--- MacOS/dock.sh
+++ MacOS/dock.sh
@@ -1,16 +1,13 @@
 dockutil --remove all
 
-dockutil --add 	/Applications/Mail.app/
+dockutil --add 	/Applications/Spark.app/
+dockutil --add 	/Applications/Utilities/Terminal.app/
 dockutil --add  "/Applications/Brave Browser.app/"
-dockutil --add  "/Applications/YNAB 4.app/"
 dockutil --add 	/Applications/Spotify.app/
 dockutil --add 	/Applications/Calendar.app/
 dockutil --add 	/Applications/Bear.app/
-dockutil --add  "/Applications/App Store.app/"
-dockutil --add 	/Applications/Utilities/Terminal.app/
-dockutil --add 	/Applications/Messages.app/
-dockutil --add 	/Applications/Quiver.app/
 dockutil --add  "/Applications/Visual Studio Code.app/"
-dockutil --add 	/Applications/WhatsApp.app/
 dockutil --add 	/Applications/Todoist.app/
 dockutil --add 	/Applications/Notion.app/
+dockutil --add 	/Applications/WhatsApp.app/
+dockutil --add 	/Applications/Discord.app/
